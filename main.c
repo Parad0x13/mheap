03e55b3da452eb139fc38ed894a19a044394a812
Bryan <NONE>
2024-01-10 02:41:25 -0700

    Added more functionality. Turns out what I THOUGHT this dude in the YouTube video was doing was creating a memory management system like memory heap but maybe he wasn't. I don't know. Anyways this project can go several directions I suppose

--- main.c
+++ main.c
@@ -15,8 +15,30 @@
 // erno codes: https://stackoverflow.com/questions/46013418/how-to-check-the-value-of-errno
 #include <errno.h>
 
-void *heapAlloc(int heapSize) {
+#include <stdbool.h>
+
+// [TODO] Add munmap at some point
+// [NOTE] Should I only allocate "page aligned" values of requested size?
+
+struct heapNode_t {
+    void *head;
+    int size;
+};
+
+struct heapinfo_t {
+    bool success;
+    void *head;
+
+    struct heapNode_t *nodes;
+};
+
+struct heapinfo_t heapAlloc(int heapSize) {
     printf("Attempting to allocate a heap of size %d\n", heapSize);
+
+    struct heapinfo_t retVal;
+    retVal.success = false;
+    retVal.head = NULL;
+    retVal.nodes = NULL;
 
     void *address = NULL;
     int prot = PROT_EXEC | PROT_READ | PROT_WRITE;    // ALL THE PERMISSIONS!!!
@@ -24,15 +46,22 @@ void *heapAlloc(int heapSize) {
     int fd = 0;
     int offset = 0;
 
-    void *ptr = mmap(address, heapSize * sizeof(int), prot, flags, fd, offset);
-    printf("debug: %p\n", ptr);
+    retVal.head = mmap(address, heapSize * sizeof(int), prot, flags, fd, offset);
 
-    if (ptr == MAP_FAILED) {
+    if (retVal.head == MAP_FAILED) {
         printf("Mapping failed with error %d\n", errno);
-        return NULL;
+        return retVal;
     }
 
-    return ptr;
+    // I hate that this isn't cpp... I want classes not structure management nonsense...
+
+    retVal.success = true;
+
+    return retVal;
+}
+
+void heapPrint(void *heap) {
+    //
 }
 
 int main() {
@@ -40,8 +69,8 @@ int main() {
     printf("How many bytes do you want your heap to be?\n");
     scanf("%d", &heapSize);
 
-    void *heapPtr = heapAlloc(heapSize);
-    printf("Your heap is located at %p\n", heapPtr);
+    struct heapinfo_t heap = heapAlloc(heapSize);
+    printf("Your heap is located at %p\n", heap.head);
 
     return 0;
 }
